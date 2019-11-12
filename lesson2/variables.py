e28598f10067704f6ee3cb1a75104795d7005b63
Pav9551 <56365427+Pav9551@users.noreply.github.com>
2019-11-12 23:37:17 +0300

    Create variables.py

---
+++ lesson2/variables.py
@@ -0,0 +1,16 @@
+#Минимально 4 переменные для типов (строка, число, число с плавающей точкой, логический тип)
+
+fruit_name = 'apple'
+print(type(fruit_name))
+
+color = 'green' # цвет
+print(type(color))
+
+weight = 1.5  # вес
+print(type(weight))
+
+amount = 10   # количество
+print(type(amount))
+
+sweet = True # сладкое
+print(type(sweet))
