09141fa3b7fe932e705e4da211516fba20acb801
pmorozov <pav9551@yandex.ru>
2019-11-14 10:56:29 +0200

    add borndayforewer.py add bornyearforewer.py

---
+++ lesson2/borndayforewer.py
@@ -0,0 +1,7 @@
+year_of_birth = 1000
+day_of_birth = "100"
+while int(year_of_birth) != 1799:
+    year_of_birth = input('Введите год рождения А.С.Пушкина:  ')
+while day_of_birth != "06.06":
+    day_of_birth = input('Введите день рождения А.С.Пушкина:  ')
+print('Верно')
---
+++ lesson2/bornyearforewer.py
@@ -0,0 +1,4 @@
+year_of_birth = 1000
+while int(year_of_birth) != 1799:
+    year_of_birth = input('Введите год рождения А.С.Пушкина:  ')
+print('Верно')
