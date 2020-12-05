c9d1a4818523f4d562e079e845df3f41a3c87b4f
Pavel <pav9551@yandex.ru>
2020-12-05 21:24:58 +0300

    2seq.py ready

--- lesson3/2seq.py
+++ lesson3/2seq.py
@@ -1,30 +1,27 @@
-user_str = input('''Введите элементы списка через любой односимвольный разделитель 
-                 (разделителем будет выбран первый символ не являющийся числом или буквой): ''')
-# разделитель по умолчанию
-sep = ','
-# проверим, может пользователь использует другой разделитель
-for element in user_str:
-    if not element.isalnum():
-        sep = element
-        print(f'Определен символ-разделитель: {sep}')
-        break
-# разберем строку по кусочкам и запишем в список
-user_list = [int(i) for i in user_str.split(sep)]
-# из списка сделаем множество, чтобы убрать повторяющиеся элементы
-user_set = set(user_list)
-# тепеь у нас есть сет все варианты которые вводил пользователь
-# соберем из них список уникальных
-# для этого делаем пустой список
-result_list = []
-# и пробегаемся по всем элементам сета
-for element in user_set:
-    # если количество вхождений равно 1 то добавим в итоговый список
-    if user_list.count(element) == 1:
-        result_list.append(element)
-# выводим результат
-print(result_list)
+input_list = input('Введите элементы списка через запятую, двоеточее или слеш:')
+'''input_list = '1;2,3/4,4,3'
+input_list = '1;2;3;4;4/3e'
+input_list = '1;2;3;4;4;3'
+input_list = '1,2,3,4,4,3'
+input_list = '1/2/3/4/4/3'''
+in_status = 0;#переменная признака правильности ввода данных
+input_list_rep1 = input_list.replace(';',',')#заменяем двоеточее
+input_list_good = input_list_rep1.replace('/',',')#заменяем слеша
+if input_list.find(',', 0,len(input_list)) > 0:
+    in_status = in_status + 1#присутствует запятая
+if input_list.find(';', 0,len(input_list)) > 0:
+    in_status = in_status + 1#присутствует двоеточее
+if input_list.find('/', 0,len(input_list)) > 0:
+    in_status = in_status + 1#присутствует слеш
+if (in_status == 1):#если присутствует только один из знаков, то выдаем результат
+    set_of_num = set(input_list_good.split(','))#перобразуем список во множество
+    print(f'Список уникальных элементов: {list(set_of_num)}')# выводим результат как список
+else:
+    print('так нельзя...')
 
-input_list = input('Введите элементы списка через разделитель')
+
+
+
 
 
 
