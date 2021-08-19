ad5dc98bed82c2422740f7141b064fe765f109d4
Pav9551 <pav9551@yandex.ru>
2021-08-19 22:03:41 +0300

    Added IFTTT

---
+++ lesson12/IFTTT.py
@@ -0,0 +1,24 @@
+AIRTABLE_BASE_ID = 'appbtZQhnZqNKg4FB'
+AIRTABLE_NAME_ID = 'Table%201'
+AIRTABLE_API_KEY = 'keyubxjXqAckKCoY0'
+
+endpoint = f'https://maker.ifttt.com/trigger/1/with/key/fXHXLeRKLkPLoOgv_z0cl7QoasGw1WDW9lbrmq9QTaa'
+#python requests headers
+
+
+
+
+headers = {'Content-Type': 'application/json'}
+v1 = 1
+
+data = {"value1":v1,"value2":v1,"value3":v1}
+
+#http methods?
+#whet is the method for "create" -> HTTP Method POST
+import requests
+
+
+r = requests.post(endpoint, json = data, headers = headers)
+
+print(r.status_code)
+print(data)
